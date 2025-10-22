27757ff5f50c90d18f15373faf04f086d755c10c
Oleg Menshikov <Oleg.Menshikov@raiffeisen.ru>
2025-10-22 18:38:16 +0500

    несоклько досок2

---
+++ Canban/Migrations/20251022125448_AddBoard.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Canban.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBoard : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "BoardId",
+                table: "Tasks",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Boards",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Boards", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tasks_BoardId",
+                table: "Tasks",
+                column: "BoardId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tasks_Boards_BoardId",
+                table: "Tasks",
+                column: "BoardId",
+                principalTable: "Boards",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tasks_Boards_BoardId",
+                table: "Tasks");
+
+            migrationBuilder.DropTable(
+                name: "Boards");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Tasks_BoardId",
+                table: "Tasks");
+
+            migrationBuilder.DropColumn(
+                name: "BoardId",
+                table: "Tasks");
+        }
+    }
+}
