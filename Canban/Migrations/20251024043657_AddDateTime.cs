3714860fd9e51f23dfc538b88543ca4e62b08f71
Oleg Menshikov <Oleg.Menshikov@raiffeisen.ru>
2025-10-24 10:00:45 +0500

    Список досок2

---
+++ Canban/Migrations/20251024043657_AddDateTime.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Canban.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDateTime : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedDateTime",
+                table: "Boards",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<int>(
+                name: "Order",
+                table: "Boards",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+            
+            migrationBuilder.Sql("UPDATE Boards SET CreatedDateTime = CURRENT_TIMESTAMP");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedDateTime",
+                table: "Boards");
+
+            migrationBuilder.DropColumn(
+                name: "Order",
+                table: "Boards");
+        }
+    }
+}
